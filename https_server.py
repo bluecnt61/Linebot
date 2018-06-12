affcb39278ac79a7203ba33c24ba095ecf892d54
Eric Chang <bluecnt@gmail.com>
2018-06-12 09:33:44 +0800

    [Https]: Modify https_server.py for CA key
    The code is sample code, lets https with CA key.
    
    Test Done: Local test.
    Feature:
    Tracker: #
    Bug: #
    
    Signed-off-by: Eric Chang <bluecnt@gmail.com>

--- https_server.py
+++ https_server.py
@@ -5,9 +5,38 @@
 # then in your browser, visit:
 #    https://localhost:4443
 
-import http.server
+#!/usr/bin/env python
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import ssl
 
-httpd = http.server.HTTPServer(('localhost', 4443), http.server.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket(httpd.socket, certfile='./fullchain.pem', server_side=True)
-httpd.serve_forever()
+# HTTPRequestHandler class
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+
+    # GET
+    def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+
+        # Send headers
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        # Send message back to client
+        message = "Hello world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
+def run():
+    print('starting server...')
+    server_address = ('skyone.s7sky.com', 8081)
+    httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+
+    httpd.socket = ssl.wrap_socket (httpd.socket,
+        keyfile="/home/eric/python/https_server/privkey.pem",
+        certfile='/home/eric/python/https_server/fullchain.pem', server_side=True)
+    httpd.serve_forever()
+
+
+run()
